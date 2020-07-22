07757108d5264bf03bd0b5a1a84f46719e8c6034
rjmast <57215913+rjmast@users.noreply.github.com>
2020-07-22 18:52:18 +0530

    Add files via upload

---
+++ Windows10ISOdownloader.py
@@ -0,0 +1,56 @@
+import urllib.request, sys, os
+from sm import get_latest_links
+
+def chunk_report(bytes_so_far, chunk_size, total_size):
+   percent = float(bytes_so_far) / total_size
+   percent = round(percent*100, 2)
+   sys.stdout.write("Progress: %f of %f gigabytes (%0.2f%%)\r" %
+       (bytes_so_far*10**-9, total_size*10**-9, percent))
+   if bytes_so_far >= total_size:
+      sys.stdout.write('\n')
+
+def chunk_read(response, filename="Windows10.iso", chunk_size=1024, report_hook=None):
+   total_size = response.headers.get('Content-Length').strip()
+   total_size = int(total_size)
+   bytes_so_far = 0
+   with open(filename, "wb") as f:
+       while True:
+          chunk = response.read(chunk_size)
+          f.write(chunk)
+          bytes_so_far += len(chunk)
+          if not chunk:
+             break
+          if report_hook:
+             report_hook(bytes_so_far, chunk_size, total_size)
+
+if __name__ == '__main__':
+    ## get latest url from Microsoft
+    print("Please wait.. fetching latest download links from Microsoft...")
+    print("==============================================================")
+    print("Ignore Selenium warnings/errors")
+    print("==============================================================")
+    bit32, bit64 = get_latest_links()
+    print("links valid for 24 hours:\n", bit32, "\n", bit64, "\n")
+
+    bit_type = input("Please enter (1) 32-bit ISO, (2) 64-bit ISO: ")
+    if bit_type == '1':
+        url = bit32
+    elif bit_type == '2':
+        url = bit64
+    else:
+        exit
+
+    filename = url.split('/')[4].split('?')[0]
+    req = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        })
+    if os.path.isfile(filename):
+        print("File already exists with filename:", filename, "- press any key to override or exit to cancel.")
+        input()
+
+    response = urllib.request.urlopen(req);
+    print("Saving Windows ISO.. Filename:", filename)
+    chunk_read(response, filename, report_hook=chunk_report)  # reports on download progress
---
+++ sm.py
@@ -0,0 +1,49 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+
+def get_latest_links():
+    opts = Options()
+    opts.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1")
+    opts.add_argument("--headless")
+    opts.add_argument("window-size=1920,1080")
+    chromedriver = 'chromedriver.exe'
+    browser = webdriver.Chrome(chromedriver, chrome_options=opts)
+
+    browser.get('https://www.microsoft.com/en-gb/software-download/windows10ISO')
+    edition_dropdown = browser.find_element_by_id('product-edition')
+    edition_options = edition_dropdown.find_elements_by_tag_name("option")
+
+    for option in edition_options:
+        browser.execute_script("arguments[0].setAttribute('value','1429')", option)
+        #browser.execute_script("arguments[0].innerText = '1429'", option)
+        option.click()
+
+    browser.find_element_by_id('submit-product-edition').click()
+    language_dropdown = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "product-languages"))
+        )
+    language_dropdown = browser.find_element_by_id('product-languages')
+    language_options = language_dropdown.find_elements_by_tag_name("option")
+    for option in language_options:
+        try:
+            if json.loads(option.get_attribute("value"))["language"] == "English":
+                option.click()
+        except:
+            pass
+
+    browser.find_element_by_id('submit-sku').click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@class='button button-long button-flat button-purple']")))
+    download_btns = browser.find_elements(By.XPATH, "//*[@class='button button-long button-flat button-purple']")
+    links = [btn.get_attribute('href') for btn in download_btns]
+    bit32 = [l for l in links if "x32.iso" in l][0]
+    bit64 = [l for l in links if "x64.iso" in l][0]
+    browser.close()
+    print(bit64)
+    return bit32, bit64
+get_latest_links()
